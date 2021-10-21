b925ae1f74ed47c795549e01e87dc6f3d1fcb22e
Rehman Ali <rehmanali.9442289@gmail.com>
2021-10-21 16:07:29 +0500

    iv

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="proxy_reader",
-    version="2.0.1",
+    version="2.0.2",
     author="Rehman Ali",
     author_email="rehmanali.9442289@gmail.com",
     description="A Simple tool to read different types of proxies",
