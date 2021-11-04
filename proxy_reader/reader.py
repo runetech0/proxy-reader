0ef261ed9a02417cd7e5fa7e95ad19d4147576e2
Rehman Ali <rehmanali.9442289@gmail.com>
2021-11-04 19:06:43 +0500

    Update check domain

--- proxy_reader/reader.py
+++ proxy_reader/reader.py
@@ -123,7 +123,7 @@ class ReadProxies:
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
             urllib.request.install_opener(opener)
             # change the url address here
-            urllib.request.urlopen('https://www.myip.com/')
+            urllib.request.urlopen('https://www.example.com')
         except urllib.error.HTTPError as e:
             return False
         except Exception:
